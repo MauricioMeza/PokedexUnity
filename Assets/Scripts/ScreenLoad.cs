08b10371e1fc86812c8e5ab0a780459fb301263b
Mauricio Meza <mmezab@unal.edu.co>
2022-02-09 02:32:56 -0500

    initial poke details in text

--- Assets/Scripts/ScreenLoad.cs
+++ Assets/Scripts/ScreenLoad.cs
@@ -2,14 +2,31 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Networking;
+using TMPro;
 
 public class ScreenLoad : MonoBehaviour
 {
-    public List<Pokemon> Pokemones;
+
+    public GameObject ui_name;
+    public GameObject ui_height;
+    public GameObject ui_weight;
+    TextMeshProUGUI text_name;
+    TextMeshProUGUI text_height;
+    TextMeshProUGUI text_weight;
+
+    public List<Pokemon> pokemones;
+    public int curentPoke = 0;
 
     // Start is called before the first frame update
     void Start()
     {
+        pokemones = new List<Pokemon>();
+
+        //Get all UI elements Text Component
+        text_name = ui_name.GetComponent<TextMeshProUGUI>();
+        text_height = ui_height.GetComponent<TextMeshProUGUI>();
+        text_weight = ui_weight.GetComponent<TextMeshProUGUI>();
+
         StartCoroutine(GetPokemon("chikorita"));
     }
 
@@ -37,10 +54,23 @@ public class ScreenLoad : MonoBehaviour
         {
             //Extract JSON Data from response  
             poke = JsonUtility.FromJson<Pokemon>(getPoke.downloadHandler.text);
-            Pokemones.add(poke);
+            pokemones.Add(poke);
+            loadPokeToScreen(curentPoke);
         }
-
     } 
 
-
+    public void loadPokeToScreen(int num){
+        Pokemon currentPoke = pokemones[num];
+        
+        //Format Text
+        string n = currentPoke.name;
+        n = char.ToUpper(n[0]) + n.Substring(1);
+        float w = (float)currentPoke.weight / 10;
+        float h = (float)currentPoke.height / 10;
+        
+        //Set Text
+        text_name.text = n + " #" + currentPoke.id;
+        text_height.text = h.ToString() + " m";
+        text_weight.text = w.ToString() + " kg";
+    }
 }
