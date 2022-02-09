5b1efbb2489776675902f17ac9549d9475da30b8
Mauricio Meza <mmezab@unal.edu.co>
2022-02-09 12:55:37 -0500

    initial implementation of pokemon load change

--- Assets/Scripts/ScreenLoad.cs
+++ Assets/Scripts/ScreenLoad.cs
@@ -26,7 +26,7 @@ public class ScreenLoad : MonoBehaviour
     List<Slider> slider_stats;
 
     List<Pokemon> pokemones;
-    int curentPoke = 0;
+    int currentPoke = 0;
 
     // Start is called before the first frame update
     void Start()
@@ -48,13 +48,24 @@ public class ScreenLoad : MonoBehaviour
             slider_stats.Add(ui_s.GetComponent<Slider>());
         }
 
-        StartCoroutine(GetPokemon("shuckle"));
+        StartCoroutine(GetPokemon("charizard"));
+        StartCoroutine(GetPokemon("pikachu"));
+        StartCoroutine(GetPokemon("chikorita"));
+        
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if(Input.GetKeyDown(KeyCode.RightArrow)){
+            
+            if(currentPoke != (pokemones.Count-1))
+                currentPoke++;
+            else
+                currentPoke = 0;
+
+            loadPokeToScreen(currentPoke);
+        }  
     }
 
 
@@ -75,14 +86,14 @@ public class ScreenLoad : MonoBehaviour
             //Extract JSON Data from response  
             poke = JsonUtility.FromJson<Pokemon>(www.downloadHandler.text);
             pokemones.Add(poke);
-            loadPokeToScreen(curentPoke);
+            StartCoroutine(GetPokeSprite(poke));
         }
     } 
 
 
-    IEnumerator GetPokeSprite(string spriteURL)
+    IEnumerator GetPokeSprite(Pokemon poke)
     {
-
+        string spriteURL = poke.sprites.front_default;
         UnityWebRequest www = UnityWebRequestTexture.GetTexture(spriteURL);
         yield return www.SendWebRequest();
 
@@ -97,7 +108,8 @@ public class ScreenLoad : MonoBehaviour
             Sprite sprite = Sprite.Create((Texture2D)texture, 
             new Rect(0,0, texture.width, texture.height),      
             Vector2.one/2);
-            img.sprite = sprite;
+            poke.sprite = sprite;
+            loadPokeToScreen(currentPoke);
         }
     } 
 
@@ -109,7 +121,9 @@ public class ScreenLoad : MonoBehaviour
         float w = (float)currentPoke.weight / 10;
         float h = (float)currentPoke.height / 10;
         
-        //Set Text in name, weight, height
+
+        //Set Img andText in name, weight, height
+        img.sprite = currentPoke.sprite;
         text_name.text = CapitalizeFirst(currentPoke.name) + " #" + currentPoke.id;
         text_height.text = h.ToString() + " m";
         text_weight.text = w.ToString() + " kg";
@@ -122,7 +136,7 @@ public class ScreenLoad : MonoBehaviour
             ui_type2.SetActive(false);
             string type = currentPoke.types[0].type.name;
             text_type1.text = CapitalizeFirst(type);
-            img_type1.color = GetColorFromType(type);
+            img_type1.color = Pokemon.GetColorFromType(type);
         }else{
             RectTransform rect;
             rect = ui_type1.GetComponent<RectTransform>();
@@ -131,16 +145,14 @@ public class ScreenLoad : MonoBehaviour
             ui_type2.SetActive(true);
             string type1 = currentPoke.types[0].type.name;
             string type2 = currentPoke.types[1].type.name;
-            Debug.Log(type1);
-            Debug.Log(type2);
             text_type1.text = CapitalizeFirst(type1);
             text_type2.text = CapitalizeFirst(type2);
-            img_type1.color = GetColorFromType(type1);
-            img_type2.color = GetColorFromType(type2);
+            img_type1.color = Pokemon.GetColorFromType(type1);
+            img_type2.color = Pokemon.GetColorFromType(type2);
             
         }
 
-        StartCoroutine(GetPokeSprite(currentPoke.sprites.front_default));
+        
 
         //Set Text, change value and change color on stats
         int i = 0;
@@ -148,7 +160,7 @@ public class ScreenLoad : MonoBehaviour
         {
             int val = currentPoke.stats[i].base_stat;
             sld.value = val;
-            sld.gameObject.transform.Find("Fill Area").Find("Fill").GetComponent<Image>().color = GetColorFromStat(val);
+            sld.gameObject.transform.Find("Fill Area").Find("Fill").GetComponent<Image>().color = Pokemon.GetColorFromStat(val);
             sld.gameObject.transform.Find("Handle Slide Area").Find("Handle").Find("Value").GetComponent<Text>().text = val.ToString();
             i++;
         }
@@ -158,90 +170,5 @@ public class ScreenLoad : MonoBehaviour
     private string CapitalizeFirst(string str){
         str = char.ToUpper(str[0]) + str.Substring(1);
         return str;
-    }
-
-
-    private Color GetColorFromStat(int value){
-        if(value >= 0 && value < 10){
-            return Color.red;
-        }
-        else if(value >= 10 && value < 50){
-            return new Color(1, (float)0.5, 0);
-        }
-        else if(value >= 50 && value < 100){
-            return Color.yellow;
-        }
-        else if(value >= 100 && value < 150){
-            return new Color((float)0.25, 1, 0);
-        }
-        else if(value >= 150 && value < 200){
-            return Color.green;
-        }
-        else if(value >= 200)
-        {
-            return new Color(0, (float)0.75, 0);
-        }
-        else
-        {
-            return Color.black;
-        }
-    }
-
-    private Color GetColorFromType(string type)
-    {
-        Color color;
-        switch(type)
-        {
-            case "grass":
-                color = new Color((float)0.48, (float)0.78, (float)0.3);
-                break;
-            case "normal":
-                color = new Color((float)0.66, (float)0.65, (float)0.48);
-                break;
-            case "fire":
-                color = new Color((float)0.93, (float)0.51, (float)0.19);
-                break;
-            case "water":
-                color = new Color((float)0.39, (float)0.56, (float)0.94);
-                break;
-            case "electric":
-                color = new Color((float)0.97, (float)0.82, (float)0.17);
-                break;
-            case "ice":
-                color = new Color((float)0.39, (float)0.56, (float)0.60);
-                break;
-            case "fighting":
-                color = new Color((float)0.66, (float)0.65, (float)0.48);
-                break;
-            case "poison":
-                color = new Color((float)0.64, (float)0.24, (float)0.63);
-                break;
-            case "ground":
-                color = new Color((float)0.89, (float)0.75, (float)0.40);
-                break;
-            case "psychic":
-                color = Color.magenta;
-                break;
-            case "bug":
-                color = new Color((float)0.48, (float)0.5, (float)0.3);
-                break;
-            case "dragon":
-                color = new Color((float)0.44, (float)0.21, (float)0.99);
-                break;
-            case "dark":
-                color = Color.black;
-                break;
-            case "steel":
-                color = Color.gray;
-                break;
-            case "fairy":
-                color = new Color((float)0.84, (float)0.51, (float)0.68);
-                break;
-            default:
-                color = Color.white;
-                break;
-        }
-
-        return color;   
     }
 }
